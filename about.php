1ef8b019a6306c44499f1a02596d1efebbe19f5b
Randy Steven <randysteven12@gmail.com>
2024-06-18 14:35:00 +0700

    refactoring about and product

--- about.php
+++ about.php
@@ -1,12 +1,5 @@
 <?php require_once "./header.php"; ?>
 <div class="box">
-    <div>
-        <h3>Skripsi</h3>
-        <p>Rp 75.000,-/jam</p>
-    </div>
-    <div>
-        <h3>Backend</h3>
-        <p>Rp 800.000,-/API</p>
-    </div>
+
 </div>
 <?php require_once "./footer.php"; ?>
---
+++ product.php
@@ -0,0 +1,16 @@
+<?php require_once "./header.php";?>
+<div class="box">
+    <div>
+        <h3>Konsultasi Skripsi</h3>
+        <p>Rp 75.000,-/jam</p>
+    </div>
+    <div>
+        <h3>Backend</h3>
+        <p>Rp 800.000,-/API</p>
+    </div>
+    <div>
+        <h3>Mock Interview</h3>
+        <p>Rp 250.000,-/Session</p>
+    </div>
+</div>
+<?php require_once "./footer.php";?>
